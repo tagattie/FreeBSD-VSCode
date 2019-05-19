983ed2954783d91e56436f6cbf73ad2cb08b0123
Hiroki Tagato <tagattie@gmail.com>
2019-05-19 22:01:55 +0900

    Add UI smoke tests to test target.

--- editors/vscode/files/patch-build_npm_postinstall.js
+++ editors/vscode/files/patch-build_npm_postinstall.js
@@ -14,15 +14,6 @@
  	console.log('Installing dependencies in \'%s\'.', location);
  	const result = cp.spawnSync(yarn, args, opts);
  
-@@ -69,7 +76,7 @@ runtime "${runtime}"`;
- }
- 
- yarnInstall(`build`); // node modules required for build
--yarnInstall('test/smoke'); // node modules required for smoketest
-+// yarnInstall('test/smoke'); // node modules required for smoketest
- yarnInstallBuildDependencies(); // node modules for watching, specific to host node version, not electron
- 
- // Remove the windows process tree typings as this causes duplicate identifier errors in tsc builds
 @@ -77,4 +84,4 @@ const processTreeDts = path.join('node_modules', 'wind
  if (fs.existsSync(processTreeDts)) {
  	console.log('Removing windows-process-tree.d.ts');
