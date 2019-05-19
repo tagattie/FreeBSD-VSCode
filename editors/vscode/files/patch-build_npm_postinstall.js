0e600c5d5ec9806183706f27bac4ab3d0fd40087
Hiroki Tagato <tagattie@gmail.com>
2019-05-19 21:09:03 +0900

    Pass --frozen-lockfile and --offline to subsequently called yarnInstall()'s on yarn install.

--- editors/vscode/files/patch-build_npm_postinstall.js
+++ editors/vscode/files/patch-build_npm_postinstall.js
@@ -1,6 +1,20 @@
---- build/npm/postinstall.js.orig	2019-03-20 08:48:14 UTC
+--- build/npm/postinstall.js.orig	2019-05-17 20:03:55 UTC
 +++ build/npm/postinstall.js
-@@ -69,7 +69,7 @@ runtime "${runtime}"`;
+@@ -26,6 +26,13 @@ function yarnInstall(location, opts) {
+ 		args.push('--ignore-optional');
+ 	}
+ 
++	const addargs = ['--frozen-lockfile', '--offline', '--no-progress', '--verbose'];
++	addargs.forEach(arg => {
++		if (original.indexOf(arg) > -1) {
++			args.push(arg)
++		}
++	});
++
+ 	console.log('Installing dependencies in \'%s\'.', location);
+ 	const result = cp.spawnSync(yarn, args, opts);
+ 
+@@ -69,7 +76,7 @@ runtime "${runtime}"`;
  }
  
  yarnInstall(`build`); // node modules required for build
@@ -9,7 +23,7 @@
  yarnInstallBuildDependencies(); // node modules for watching, specific to host node version, not electron
  
  // Remove the windows process tree typings as this causes duplicate identifier errors in tsc builds
-@@ -77,4 +77,4 @@ const processTreeDts = path.join('node_modules', 'wind
+@@ -77,4 +84,4 @@ const processTreeDts = path.join('node_modules', 'wind
  if (fs.existsSync(processTreeDts)) {
  	console.log('Removing windows-process-tree.d.ts');
  	fs.unlinkSync(processTreeDts);
