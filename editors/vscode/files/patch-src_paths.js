19c67f97226ffd97a02176e2ba2660c558da0390
Hiroki Tagato <tagattie@gmail.com>
2019-05-20 10:00:16 +0900

    Simplify patches.

--- editors/vscode/files/patch-src_paths.js
+++ editors/vscode/files/patch-src_paths.js
@@ -1,14 +1,17 @@
---- src/paths.js.orig	2019-03-20 08:48:14 UTC
+--- src/paths.js.orig	2019-05-17 20:03:55 UTC
 +++ src/paths.js
-@@ -20,6 +20,7 @@ function getAppDataPath(platform) {
+@@ -19,7 +19,9 @@ function getAppDataPath(platform) {
+ 	switch (platform) {
  		case 'win32': return process.env['VSCODE_APPDATA'] || process.env['APPDATA'] || path.join(process.env['USERPROFILE'], 'AppData', 'Roaming');
  		case 'darwin': return process.env['VSCODE_APPDATA'] || path.join(os.homedir(), 'Library', 'Application Support');
- 		case 'linux': return process.env['VSCODE_APPDATA'] || process.env['XDG_CONFIG_HOME'] || path.join(os.homedir(), '.config');
-+		case 'freebsd': return process.env['VSCODE_APPDATA'] || process.env['XDG_CONFIG_HOME'] || path.join(os.homedir(), '.config');
+-		case 'linux': return process.env['VSCODE_APPDATA'] || process.env['XDG_CONFIG_HOME'] || path.join(os.homedir(), '.config');
++		case 'linux':
++		case 'freebsd':
++			return process.env['VSCODE_APPDATA'] || process.env['XDG_CONFIG_HOME'] || path.join(os.homedir(), '.config');
  		default: throw new Error('Platform not supported');
  	}
  }
-@@ -33,4 +34,4 @@ function getDefaultUserDataPath(platform) {
+@@ -33,4 +35,4 @@ function getDefaultUserDataPath(platform) {
  }
  
  exports.getAppDataPath = getAppDataPath;
--- editors/vscode/files/patch-src_vs_workbench_contrib_extensions_electron-browser_extensionEditor.ts
+++ editors/vscode/files/patch-src_vs_workbench_contrib_extensions_electron-browser_extensionEditor.ts
@@ -1,10 +1,13 @@
 --- src/vs/workbench/contrib/extensions/electron-browser/extensionEditor.ts.orig	2019-05-17 20:03:55 UTC
 +++ src/vs/workbench/contrib/extensions/electron-browser/extensionEditor.ts
-@@ -1132,6 +1132,7 @@ export class ExtensionEditor extends BaseEditor {
+@@ -1131,7 +1131,9 @@ export class ExtensionEditor extends BaseEditor {
+ 
  		switch (process.platform) {
  			case 'win32': key = rawKeyBinding.win; break;
- 			case 'linux': key = rawKeyBinding.linux; break;
-+			case 'freebsd': key = rawKeyBinding.linux; break;
+-			case 'linux': key = rawKeyBinding.linux; break;
++			case 'linux':
++			case 'freebsd':
++				key = rawKeyBinding.linux; break;
  			case 'darwin': key = rawKeyBinding.mac; break;
  		}
  
