b2d566afed0040e24983ff128cc1f5da7f78c91f
Hiroki Tagato <tagattie@gmail.com>
2019-06-18 20:58:28 +0900

    Temporarily disable UI smoke test due to electron versioning issue.

--- editors/vscode/files/patch-build_npm_postinstall.js
+++ editors/vscode/files/patch-build_npm_postinstall.js
@@ -14,6 +14,15 @@
  	console.log('Installing dependencies in \'%s\'.', location);
  	const result = cp.spawnSync(yarn, args, opts);
  
+@@ -71,7 +78,7 @@ runtime "${runtime}"`;
+ }
+ 
+ yarnInstall(`build`); // node modules required for build
+-yarnInstall('test/smoke'); // node modules required for smoketest
++// yarnInstall('test/smoke'); // node modules required for smoketest
+ yarnInstallBuildDependencies(); // node modules for watching, specific to host node version, not electron
+ 
+ // Remove the windows process tree typings as this causes duplicate identifier errors in tsc builds
 @@ -79,4 +86,4 @@ const processTreeDts = path.join('node_modules', 'wind
  if (fs.existsSync(processTreeDts)) {
  	console.log('Removing windows-process-tree.d.ts');
