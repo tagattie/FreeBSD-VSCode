c5ac3e89994c2e3410dd9811669b2bf2daa4a18b
Hiroki Tagato <tagattie@gmail.com>
2026-07-17 17:55:27 +0900

    Update to 1.129.0
    Changelog: https://code.visualstudio.com/updates/v1_129

--- editors/vscode/files/patch-src_bootstrap-node.ts
+++ editors/vscode/files/patch-src_bootstrap-node.ts
@@ -1,6 +1,6 @@
---- src/bootstrap-node.ts.orig	2025-12-10 18:02:22 UTC
+--- src/bootstrap-node.ts.orig	2026-07-15 00:08:44 UTC
 +++ src/bootstrap-node.ts
-@@ -155,7 +155,7 @@ export function configurePortable(product: Partial<IPr
+@@ -221,7 +221,7 @@ export function configurePortable(product: Partial<IPr
  			return process.env['VSCODE_PORTABLE'];
  		}
  
